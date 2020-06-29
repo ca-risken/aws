fabaa151b04e16fa33f738c8de949ab7957e56e9
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2020-06-29 14:02:22 +0900

    fix message

--- pkg/message/message.go
+++ pkg/message/message.go
@@ -1,4 +1,4 @@
-package main
+package message
 
 import validation "github.com/go-ozzo/ozzo-validation"
 
--- pkg/message/message_test.go
+++ pkg/message/message_test.go
@@ -1,51 +1,51 @@
-package main
+package message
 
 import "testing"
 
 func TestValidate(t *testing.T) {
 	cases := []struct {
 		name    string
-		input   *guardDutyMessage
+		input   *GuardDutyMessage
 		wantErr bool
 	}{
 		{
 			name:  "OK",
-			input: &guardDutyMessage{DataSource: "aws:guard-duty", ProjectID: 1, AccountID: "123456789012", AssumeRoleArn: "role", ExternalID: ""},
+			input: &GuardDutyMessage{DataSource: "aws:guard-duty", ProjectID: 1, AccountID: "123456789012", AssumeRoleArn: "role", ExternalID: ""},
 		},
 		{
 			name:    "NG Required(DataSource)",
-			input:   &guardDutyMessage{DataSource: "", ProjectID: 1, AccountID: "123456789012", AssumeRoleArn: "role", ExternalID: ""},
+			input:   &GuardDutyMessage{DataSource: "", ProjectID: 1, AccountID: "123456789012", AssumeRoleArn: "role", ExternalID: ""},
 			wantErr: true,
 		},
 		{
 			name:    "NG Unknown(DataSource)",
-			input:   &guardDutyMessage{DataSource: "aws:guard-duty-x", ProjectID: 1, AccountID: "123456789012", AssumeRoleArn: "role", ExternalID: ""},
+			input:   &GuardDutyMessage{DataSource: "aws:guard-duty-x", ProjectID: 1, AccountID: "123456789012", AssumeRoleArn: "role", ExternalID: ""},
 			wantErr: true,
 		},
 		{
 			name:    "NG Required(ProjectID)",
-			input:   &guardDutyMessage{DataSource: "aws:guard-duty", ProjectID: 0, AccountID: "123456789012", AssumeRoleArn: "role", ExternalID: ""},
+			input:   &GuardDutyMessage{DataSource: "aws:guard-duty", ProjectID: 0, AccountID: "123456789012", AssumeRoleArn: "role", ExternalID: ""},
 			wantErr: true,
 		},
 		{
 			name:    "NG Required(AccountID)",
-			input:   &guardDutyMessage{DataSource: "aws:guard-duty", ProjectID: 0, AccountID: "", AssumeRoleArn: "role", ExternalID: ""},
+			input:   &GuardDutyMessage{DataSource: "aws:guard-duty", ProjectID: 0, AccountID: "", AssumeRoleArn: "role", ExternalID: ""},
 			wantErr: true,
 		},
 		{
 			name:    "NG Invalid Length(AccountID) 1",
-			input:   &guardDutyMessage{DataSource: "aws:guard-duty", ProjectID: 0, AccountID: "12345678901", AssumeRoleArn: "role", ExternalID: ""},
+			input:   &GuardDutyMessage{DataSource: "aws:guard-duty", ProjectID: 0, AccountID: "12345678901", AssumeRoleArn: "role", ExternalID: ""},
 			wantErr: true,
 		},
 		{
 			name:    "NG Invalid Length(AccountID) 2",
-			input:   &guardDutyMessage{DataSource: "aws:guard-duty", ProjectID: 0, AccountID: "1234567890123", AssumeRoleArn: "role", ExternalID: ""},
+			input:   &GuardDutyMessage{DataSource: "aws:guard-duty", ProjectID: 0, AccountID: "1234567890123", AssumeRoleArn: "role", ExternalID: ""},
 			wantErr: true,
 		},
 	}
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			err := c.input.validate()
+			err := c.input.Validate()
 			if c.wantErr && err == nil {
 				t.Fatal("Unexpected no error")
 			} else if !c.wantErr && err != nil {
