5b183254abf4cee2f2e0d583842c9c553d4c0932
iiiidaaa <oeriver@gmail.com>
2021-04-13 21:12:38 +0900

    fix aws portscan
    Nmapの実施、スキャン結果からのFinding作成を共通モジュールに置き換えました
    ARNとアカウントIDが一致しない場合スキャンをエラー終了するように修正しました

--- src/portscan/finding.go
+++ src/portscan/finding.go
@@ -6,32 +6,22 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"strings"
-
-	"github.com/vikyd/zero"
 
 	"github.com/CyberAgent/mimosa-aws/pkg/common"
 	"github.com/CyberAgent/mimosa-aws/pkg/message"
+	"github.com/CyberAgent/mimosa-common/pkg/portscan"
 	"github.com/CyberAgent/mimosa-core/proto/finding"
+	"github.com/vikyd/zero"
 )
 
-func makeFindings(results []*nmapResult, message *message.AWSQueueMessage) ([]*finding.FindingForUpsert, error) {
+func makeFindings(results []*portscan.NmapResult, message *message.AWSQueueMessage) ([]*finding.FindingForUpsert, error) {
 	var findings []*finding.FindingForUpsert
 	for _, r := range results {
-		data, err := json.Marshal(map[string]nmapResult{"data": *r})
+		data, err := json.Marshal(map[string]portscan.NmapResult{"data": *r})
 		if err != nil {
 			return nil, err
 		}
-		findings = append(findings, &finding.FindingForUpsert{
-			Description:      getDescription(r.Target, r.Protocol, r.Status, r.Port),
-			DataSource:       message.DataSource,
-			DataSourceId:     generateDataSourceID(fmt.Sprintf("%v:%v:%v", r.Target, r.Protocol, r.Port)),
-			ResourceName:     r.Arn,
-			ProjectId:        message.ProjectID,
-			OriginalScore:    getScore(r),
-			OriginalMaxScore: 10.0,
-			Data:             string(data),
-		})
+		findings = append(findings, r.GetFindings(message.ProjectID, message.DataSource, string(data))...)
 	}
 	return findings, nil
 }
@@ -93,10 +83,6 @@ func (s *sqsHandler) tagFinding(ctx context.Context, projectID uint32, findingID
 	return nil
 }
 
-func getDescription(target, protocol, status string, port int) string {
-	return fmt.Sprintf("%v is %v. protocol: %v, port %v", target, status, protocol, port)
-}
-
 func getExcludeDescription(target, protocol string, fPort, tPort int, securityGroup string) string {
 	if securityGroup != "" {
 		return fmt.Sprintf("Too many ports are exposed.target:%v protocol: %v, port %v-%v,securiry_group: %v", target, protocol, fPort, tPort, securityGroup)
@@ -107,35 +93,4 @@ func getExcludeDescription(target, protocol string, fPort, tPort int, securityGr
 func generateDataSourceID(input string) string {
 	hash := sha256.Sum256([]byte(input))
 	return hex.EncodeToString(hash[:])
-}
-
-func getScore(result *nmapResult) float32 {
-	status := result.Status
-	protocol := result.Protocol
-	port := result.Port
-	if strings.ToUpper(status) == "CLOSED" || (strings.ToUpper(protocol) == "TCP" && strings.Index(strings.ToUpper(status), "FILTERED") > -1) {
-		return 1.0
-	}
-	if strings.ToUpper(protocol) == "UDP" {
-		return 6.0
-	}
-	switch port {
-	case 22, 3306, 5432, 6379:
-		return 8.0
-	default:
-		score := getScoreByScanDetail(result.ScanDetail)
-		return score
-	}
-}
-
-func getScoreByScanDetail(detail map[string]interface{}) float32 {
-	val, ok := detail["status"]
-	if !ok {
-		return 6.0
-	}
-	if strings.Index(val.(string), "401") > -1 || strings.Index(val.(string), "403") > -1 {
-		return 1.0
-	}
-	return 6.0
-
 }
--- src/portscan/handler_test.go
+++ src/portscan/handler_test.go
@@ -4,94 +4,41 @@ import (
 	"testing"
 )
 
-func TestGetScore(t *testing.T) {
+func TestIsMatchAccountIDArn(t *testing.T) {
 	cases := []struct {
-		name   string
-		result *nmapResult
-		want   float32
+		name      string
+		arn       string
+		accountID string
+		want      bool
 	}{
 		{
-			name: "tcp critical port",
-			result: &nmapResult{
-				Protocol: "tcp",
-				Port:     3306,
-				Status:   "open",
-			},
-			want: 8.0,
+			name:      "OK Match AccountID ARN",
+			arn:       "arn:aws:iam::556822360006:role/RISKEN",
+			accountID: "556822360006",
+			want:      true,
 		},
 		{
-			name: "tcp http/https port not 401,403",
-			result: &nmapResult{
-				Protocol: "tcp",
-				Port:     443,
-				Status:   "open",
-				ScanDetail: map[string]interface{}{
-					"status": "200 OK",
-				},
-			},
-			want: 6.0,
+			name:      "NG Doesn't match AccountID ARN",
+			arn:       "arn:aws:iam::556822360006:role/RISKEN",
+			accountID: "123456789012",
+			want:      false,
 		},
 		{
-			name: "tcp http/https port 401,403",
-			result: &nmapResult{
-				Protocol: "tcp",
-				Port:     443,
-				Status:   "open",
-				ScanDetail: map[string]interface{}{
-					"status": "401 Unauthorized",
-				},
-			},
-			want: 1.0,
+			name:      "NG name likes accountID",
+			arn:       "arn:aws:iam::556822360006:role/123456789012",
+			accountID: "123456789012",
+			want:      false,
 		},
 		{
-			name: "tcp unknown port",
-			result: &nmapResult{
-				Protocol: "tcp",
-				Port:     38080,
-				Status:   "open",
-			},
-			want: 6.0,
-		},
-		{
-			name: "tcp closed critical port",
-			result: &nmapResult{
-				Protocol: "tcp",
-				Port:     3306,
-				Status:   "closed",
-			},
-			want: 1.0,
-		},
-		{
-			name: "tcp filtered critical port",
-			result: &nmapResult{
-				Protocol: "tcp",
-				Port:     5432,
-				Status:   "filtered",
-			},
-			want: 1.0,
-		},
-		{
-			name: "udp open port",
-			result: &nmapResult{
-				Protocol: "udp",
-				Port:     80,
-				Status:   "open",
-			},
-			want: 6.0,
-		},
-		{
-			name: "udp open/filtered critical port",
-			result: &nmapResult{
-				Protocol: "udp",
-				Status:   "open/filtered",
-				Port:     53,
-			},
-			want: 6.0,
+			name:      "NG invalid arn",
+			arn:       "arn:aws:iam556822360006:role/123456789012",
+			accountID: "556822360006",
+			want:      false,
 		},
 	}
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			got := getScore(c.result)
+			got := isMatchAccountIDArn(c.accountID, c.arn)
 			if c.want != got {
 				t.Fatalf("Unexpected resource name: want=%v, got=%v", c.want, got)
 			}
