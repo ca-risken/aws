ecff18d7c9d923ad4e1490aa099d7b1e65fab5e4
mmmknt <mmmknt@mmmknt.dev>
2022-02-19 04:41:48 +0900

    Make assign parameters to main

--- src/portscan/main.go
+++ src/portscan/main.go
@@ -9,12 +9,31 @@ import (
 	"github.com/gassara-kys/envconfig"
 )
 
-type serviceConfig struct {
+type AppConfig struct {
 	EnvName string `default:"local" split_words:"true"`
+
+	// sqs
+	Debug string `default:"false"`
+
+	AWSRegion   string `envconfig:"aws_region"   default:"ap-northeast-1"`
+	SQSEndpoint string `envconfig:"sqs_endpoint" default:"http://queue.middleware.svc.cluster.local:9324"`
+
+	PortscanQueueName  string `split_words:"true" default:"aws-portscan"`
+	PortscanQueueURL   string `split_words:"true" default:"http://queue.middleware.svc.cluster.local:9324/queue/aws-portscan"`
+	MaxNumberOfMessage int64  `split_words:"true" default:"5"`
+	WaitTimeSecond     int64  `split_words:"true" default:"20"`
+
+	// grpc
+	FindingSvcAddr string `required:"true" split_words:"true" default:"finding.core.svc.cluster.local:8001"`
+	AlertSvcAddr   string `required:"true" split_words:"true" default:"alert.core.svc.cluster.local:8004"`
+	AWSSvcAddr     string `required:"true" split_words:"true" default:"aws.aws.svc.cluster.local:9001"`
+
+	// portsan
+	ScanExcludePortNumber int `split_words:"true"     default:"1000"`
 }
 
 func main() {
-	var conf serviceConfig
+	var conf AppConfig
 	err := envconfig.Process("", &conf)
 	if err != nil {
 		appLogger.Fatal(err.Error())
@@ -23,12 +42,31 @@ func main() {
 	if err != nil {
 		appLogger.Fatal(err.Error())
 	}
-	ctx := context.Background()
-	consumer := newSQSConsumer()
+
+	sqsConf := &sqsConfig{
+		Debug:              conf.Debug,
+		AWSRegion:          conf.AWSRegion,
+		SQSEndpoint:        conf.SQSEndpoint,
+		PortscanQueueName:  conf.PortscanQueueName,
+		PortscanQueueURL:   conf.PortscanQueueURL,
+		MaxNumberOfMessage: conf.MaxNumberOfMessage,
+		WaitTimeSecond:     conf.WaitTimeSecond,
+	}
+	consumer := newSQSConsumer(sqsConf)
+
+	handler := &sqsHandler{
+		awsRegion:             conf.AWSRegion,
+		scanExcludePortNumber: conf.ScanExcludePortNumber,
+	}
+	handler.findingClient = newFindingClient(conf.FindingSvcAddr)
+	handler.alertClient = newAlertClient(conf.AlertSvcAddr)
+	handler.awsClient = newAWSClient(conf.AWSSvcAddr)
+
 	appLogger.Info("Start the portscan SQS consumer server...")
+	ctx := context.Background()
 	consumer.Start(ctx,
 		mimosasqs.InitializeHandler(
 			mimosasqs.RetryableErrorHandler(
 				mimosasqs.StatusLoggingHandler(appLogger,
-					mimosaxray.MessageTracingHandler(conf.EnvName, "aws.portScan", newHandler())))))
+					mimosaxray.MessageTracingHandler(conf.EnvName, "aws.portScan", handler)))))
 }
