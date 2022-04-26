bb429cb10d2e122436dd370c07f6b8280d6a138f
gassara-kys <ogasawara_kiyoshi@cybernagent>
2022-04-26 15:12:22 +0900

    migrate: aws-sdk-go-v2 (accessanalyzer)

--- src/access-analyzer/accessanalyzer.go
+++ src/access-analyzer/accessanalyzer.go
@@ -7,66 +7,64 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/accessanalyzer"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
+	"github.com/aws/aws-sdk-go-v2/service/accessanalyzer"
+	"github.com/aws/aws-sdk-go-v2/service/accessanalyzer/types"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/ca-risken/aws/pkg/message"
 	"github.com/ca-risken/core/proto/finding"
 )
 
 type accessAnalyzerAPI interface {
 	getAccessAnalyzer(ctx context.Context, msg *message.AWSQueueMessage) ([]*finding.FindingForUpsert, *[]string, error)
-	listAvailableRegion(ctx context.Context) ([]*ec2.Region, error)
+	listAvailableRegion(ctx context.Context) (*[]ec2types.Region, error)
 	listAnalyzers(ctx context.Context) (*[]string, error)
-	listFindings(ctx context.Context, accountID string, analyzerArn string) ([]*accessanalyzer.FindingSummary, error)
+	listFindings(ctx context.Context, accountID string, analyzerArn string) (*[]types.FindingSummary, error)
 }
 
 type accessAnalyzerClient struct {
-	Sess *session.Session
-	Svc  *accessanalyzer.AccessAnalyzer
-	EC2  *ec2.EC2
+	Svc *accessanalyzer.Client
+	EC2 *ec2.Client
 }
 
-func newAccessAnalyzerClient(region, assumeRole, externalID string) (accessAnalyzerAPI, error) {
+func newAccessAnalyzerClient(ctx context.Context, region, assumeRole, externalID string) (accessAnalyzerAPI, error) {
 	a := accessAnalyzerClient{}
-	if err := a.newAWSSession(region, assumeRole, externalID); err != nil {
+	if err := a.newAWSSession(ctx, region, assumeRole, externalID); err != nil {
 		return nil, err
 	}
 	return &a, nil
 }
 
-func (a *accessAnalyzerClient) newAWSSession(region, assumeRole, externalID string) error {
+func (a *accessAnalyzerClient) newAWSSession(ctx context.Context, region, assumeRole, externalID string) error {
 	if assumeRole == "" {
 		return errors.New("Required AWS AssumeRole")
 	}
-	sess, err := session.NewSession()
-	if err != nil {
-		appLogger.Errorf("Failed to create session, err=%+v", err)
-		return err
+	if externalID == "" {
+		return errors.New("Required AWS ExternalID")
 	}
-	var cred *credentials.Credentials
-	if externalID != "" {
-		cred = stscreds.NewCredentials(
-			sess, assumeRole, func(p *stscreds.AssumeRoleProvider) {
-				p.ExternalID = aws.String(externalID)
-			},
-		)
-	} else {
-		cred = stscreds.NewCredentials(sess, assumeRole)
-	}
-	sessWithCred, err := session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-		Config:            aws.Config{Region: &region, Credentials: cred},
-	})
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
 	if err != nil {
 		return err
 	}
-	a.Sess = sessWithCred
-	a.Svc = accessanalyzer.New(a.Sess, aws.NewConfig().WithRegion(region))
-	a.EC2 = ec2.New(a.Sess, aws.NewConfig().WithRegion(region))
+	stsClient := sts.NewFromConfig(cfg)
+	provider := stscreds.NewAssumeRoleProvider(stsClient, assumeRole,
+		func(p *stscreds.AssumeRoleOptions) {
+			p.RoleARN = assumeRole
+			p.RoleSessionName = "RISKEN"
+			p.ExternalID = &externalID
+		},
+	)
+	cfg.Credentials = aws.NewCredentialsCache(provider)
+	_, err = cfg.Credentials.Retrieve(ctx)
+	if err != nil {
+		return err
+	}
+	a.Svc = accessanalyzer.New(accessanalyzer.Options{Credentials: cfg.Credentials, Region: region})
+	a.EC2 = ec2.New(ec2.Options{Credentials: cfg.Credentials, Region: region})
 	return nil
 }
 
@@ -87,11 +85,11 @@ func (a *accessAnalyzerClient) getAccessAnalyzer(ctx context.Context, msg *messa
 			continue // If Organization gathering enabled, requesting an invalid Region may result in an error.
 		}
 		appLogger.Debugf("[Debug]Got findings, %+v", findings)
-		if len(findings) == 0 {
+		if findings == nil || len(*findings) == 0 {
 			appLogger.Infof("No findings: analyzerArn=%s, accountID=%s", arn, msg.AccountID)
 			continue
 		}
-		for _, data := range findings {
+		for _, data := range *findings {
 			buf, err := json.Marshal(data)
 			if err != nil {
 				appLogger.Errorf("Failed to json encoding error: err=%+v", err)
@@ -99,7 +97,6 @@ func (a *accessAnalyzerClient) getAccessAnalyzer(ctx context.Context, msg *messa
 			}
 			isPublic := false
 			if data.IsPublic != nil {
-				appLogger.Warnf("API Response parameter `IsPublic` got nil data, maybe something error occured, accountID=%s", msg.AccountID)
 				isPublic = *data.IsPublic
 			}
 			putData = append(putData, &finding.FindingForUpsert{
@@ -108,7 +105,7 @@ func (a *accessAnalyzerClient) getAccessAnalyzer(ctx context.Context, msg *messa
 				DataSourceId:     *data.Id,
 				ResourceName:     *data.Resource,
 				ProjectId:        msg.ProjectID,
-				OriginalScore:    scoreAccessAnalyzerFinding(*data.Status, isPublic, data.Action),
+				OriginalScore:    scoreAccessAnalyzerFinding(data.Status, isPublic, data.Action),
 				OriginalMaxScore: 1.0,
 				Data:             string(buf),
 			})
@@ -117,8 +114,8 @@ func (a *accessAnalyzerClient) getAccessAnalyzer(ctx context.Context, msg *messa
 	return putData, analyzerArns, nil
 }
 
-func (a *accessAnalyzerClient) listAvailableRegion(ctx context.Context) ([]*ec2.Region, error) {
-	out, err := a.EC2.DescribeRegionsWithContext(ctx, &ec2.DescribeRegionsInput{})
+func (a *accessAnalyzerClient) listAvailableRegion(ctx context.Context) (*[]ec2types.Region, error) {
+	out, err := a.EC2.DescribeRegions(ctx, &ec2.DescribeRegionsInput{})
 	if err != nil {
 		return nil, err
 	}
@@ -126,15 +123,15 @@ func (a *accessAnalyzerClient) listAvailableRegion(ctx context.Context) ([]*ec2.
 		appLogger.Warn("Got no regions")
 		return nil, nil
 	}
-	return out.Regions, nil
+	return &out.Regions, nil
 }
 
 func (a *accessAnalyzerClient) listAnalyzers(ctx context.Context) (*[]string, error) {
 	var nextToken string
 	var analyzers []string
 	for {
-		out, err := a.Svc.ListAnalyzersWithContext(ctx, &accessanalyzer.ListAnalyzersInput{
-			MaxResults: aws.Int64(50),
+		out, err := a.Svc.ListAnalyzers(ctx, &accessanalyzer.ListAnalyzersInput{
+			MaxResults: aws.Int32(50),
 			NextToken:  &nextToken,
 		})
 		if err != nil {
@@ -151,22 +148,21 @@ func (a *accessAnalyzerClient) listAnalyzers(ctx context.Context) (*[]string, er
 	return &analyzers, nil
 }
 
-func (a *accessAnalyzerClient) listFindings(ctx context.Context, accountID string, analyzerArn string) ([]*accessanalyzer.FindingSummary, error) {
+func (a *accessAnalyzerClient) listFindings(ctx context.Context, accountID string, analyzerArn string) (*[]types.FindingSummary, error) {
 	var nextToken string
 	input := &accessanalyzer.ListFindingsInput{
 		AnalyzerArn: aws.String(analyzerArn),
-		Filter: map[string]*accessanalyzer.Criterion{
-			"resourceOwnerAccount": {Eq: []*string{aws.String(accountID)}},
-			// "status":               {Eq: []*string{aws.String("ACTIVE")}},
+		Filter: map[string]types.Criterion{
+			"resourceOwnerAccount": {Eq: []string{accountID}},
 		},
-		MaxResults: aws.Int64(50),
+		MaxResults: aws.Int32(50),
 	}
-	var findings []*accessanalyzer.FindingSummary
+	var findings []types.FindingSummary
 	for {
 		if nextToken != "" {
 			input.NextToken = &nextToken
 		}
-		out, err := a.Svc.ListFindingsWithContext(ctx, input)
+		out, err := a.Svc.ListFindings(ctx, input)
 		if err != nil {
 			return nil, err
 		}
@@ -177,5 +173,5 @@ func (a *accessAnalyzerClient) listFindings(ctx context.Context, accountID strin
 		nextToken = *out.NextToken
 		time.Sleep(time.Millisecond * 500)
 	}
-	return findings, nil
+	return &findings, nil
 }
