dfd07735f7a03e89037f1f7ec8cc379466c89cbf
mmmknt <mmmknt@mmmknt.dev>
2022-07-22 16:08:14 +0900

    Return error when failed to create sqs consumer

--- src/access-analyzer/main.go
+++ src/access-analyzer/main.go
@@ -114,7 +114,10 @@ func main() {
 		MaxNumberOfMessage: conf.MaxNumberOfMessage,
 		WaitTimeSecond:     conf.WaitTimeSecond,
 	}
-	consumer := newSQSConsumer(ctx, sqsConf)
+	consumer, err := newSQSConsumer(ctx, sqsConf)
+	if err != nil {
+		appLogger.Fatalf(ctx, "Failed to create SQS consumer, err=%+v", err)
+	}
 
 	appLogger.Info(ctx, "Start the AWS AccessAnalyzer SQS consumer server...")
 	consumer.Start(ctx,
